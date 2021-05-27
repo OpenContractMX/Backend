8296fe001ce921fd119d647a93fb6308cb17c658
Jorge <jorgealbertodelgadilloalonso@gmail.com>
2021-05-27 14:22:52 -0500

    Fixed data types and ignoring TETS currency, now all currency is MXN

--- app/utils/json_formating.py
+++ app/utils/json_formating.py
@@ -4,18 +4,21 @@ def json_formating(result):
     invertion = 0
     time_differences = list()
     for entry in result:
-        json_entry = dict()
-        json_entry['id'] = entry[0]
-        json_entry['month'] = entry[1]
-        json_entry['trimester'] = entry[2]
-        json_entry['title'] = entry[3]
-        json_entry['buyer_name'] = entry[4]
-        json_entry['date'] = entry[5]
-        json_entry['amount'] = entry[6]
-        json_entry['currency'] = entry[7]
-        json['contracts'].append(json_entry)
-        invertion += entry[6]
-        time_differences.append((entry[9]-entry[8]).days)
+        if entry[7] == "TETS":
+            pass
+        else:
+            json_entry = dict()
+            json_entry['id'] = entry[0]
+            json_entry['month'] = int(entry[1])
+            json_entry['trimester'] = int(entry[2])
+            json_entry['title'] = entry[3]
+            json_entry['buyer_name'] = entry[4]
+            json_entry['date'] = entry[5]
+            json_entry['amount'] = entry[6]
+            json_entry['currency'] = entry[7]
+            json['contracts'].append(json_entry)
+            invertion += entry[6]
+            time_differences.append((entry[9]-entry[8]).days)
     json['contracts_number'] = len(result)
     json['inversion'] =  round(invertion, 2)
     json['execution_mean'] = sum(time_differences)/len(result)
