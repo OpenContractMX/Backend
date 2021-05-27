12e518d72bd47b6c1f70ce8b0702cd727ed7d5a2
Jorge <jorgealbertodelgadilloalonso@gmail.com>
2021-05-27 02:04:16 -0500

    Created the basic structure for contracts endpoint

--- app/main.py
+++ app/main.py
@@ -1,15 +1,17 @@
 from fastapi import FastAPI
 from app.core.config import settings
 from app.utils.contracts_functions import get_contracts
+from app.utils.json_formating import json_formating
 
 
 app = FastAPI(title=settings.PROJECT_TITLE, version=settings.PROJECT_VERSION)
 
 
 @app.get("/api/contracts")
-def GET_contracts():
+def GET_contracts(category: str = "comunicacion_transporte", year: int = 2016):
     try:
-        result = get_contracts()
-        return {"hi!": result}
+        result = get_contracts(category,year)
+        json = json_formating(result)
+        return {"response": json}
     except Exception as e:
         print("An error ocurred", e)
