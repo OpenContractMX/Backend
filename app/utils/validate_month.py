02dec01193fe4e220d580ef8a39477b4c039a718
Jorge <jorgealbertodelgadilloalonso@gmail.com>
2021-05-30 13:11:04 -0500

    More validations

---
+++ app/utils/validate_month.py
@@ -0,0 +1,8 @@
+def validate_month(month):
+
+    months = [0,1,2,3,4,5,6,7,8,9,10,11,12]
+
+    if month in months:
+        return True
+
+    return False
---
+++ app/utils/validate_trimester.py
@@ -0,0 +1,8 @@
+def validate_trimester(trimester):
+
+    trimesters = [0,1,2,3,4]
+
+    if trimester in trimesters:
+        return True
+
+    return False
