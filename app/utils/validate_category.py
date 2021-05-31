f9dee96a08a2df1a1649aceb1d21303395166a4b
Jorge <jorgealbertodelgadilloalonso@gmail.com>
2021-05-31 13:15:44 -0500

    fixed typo in category validation

--- app/utils/validate_category.py
+++ app/utils/validate_category.py
@@ -1,6 +1,6 @@
 def validate_category(category):
     
-    valid_categories = ["seguridad","salud","energia","economia","gobernacion","medio_ambiente","comunicacion_transporte","social","investigacion","trabajo"]
+    valid_categories = ["seguridad","educacion","salud","energia","economia","gobernacion","medio_ambiente","comunicacion_transporte","social","investigacion","trabajo"]
     
     if category in valid_categories:
         return True
