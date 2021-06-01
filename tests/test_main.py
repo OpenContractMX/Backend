e935a4e2fa915125dda27aaa99b2daa6a4b6dced
Jorge <jorgealbertodelgadilloalonso@gmail.com>
2021-06-01 13:42:04 -0500

    testin

--- tests/test_main.py
+++ tests/test_main.py
@@ -5,6 +5,12 @@ client = TestClient(app)
 
 
 def test_get_contracts():
+    
+    response = client.get(
+        "/api/contracts?category=social&year=2016&month=0&trimester=0"
+    )
+    assert response.status_code == 200
+
     response = client.get(
         "/api/contracts?category=social&year=2016&month=0&trimester=0"
     )
